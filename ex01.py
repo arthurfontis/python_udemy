164330956d64c0b020c31da5005c82d6cbd2ed36
Arthur Fontinele <arthurfontis17@gmail.com>
2025-10-07 18:17:51 -0300

    Create ex01.py

---
+++ ex01.py
@@ -0,0 +1,15 @@
+a = 'casaco'
+print(a)
+
+maiuscula = a.upper()
+print(maiuscula)
+
+minuscula = maiuscula.lower()
+print(minuscula)
+
+capital = maiuscula.capitalize()
+print(capital)
+
+metade_palavra = a[0:3]
+print(metade_palavra)
+
